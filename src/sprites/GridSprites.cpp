c8be46a66f046c23b90794b25d9588e41c1ccfad
Adise <admin@pauibanez.me>
2025-11-26 14:39:22 +0100

    did some testing and added the base types and files

---
+++ src/sprites/GridSprites.cpp
@@ -0,0 +1,9 @@
+#include "GridSprites.h"
+#include <string>
+
+namespace Snake {
+namespace Sprites {
+const string GridSprites::gridSprite1 = "::::\n::::";
+const string GridSprites::gridSprite2 = "====\n====";
+} // namespace Sprites
+} // namespace Snake
---
+++ src/sprites/GridSprites.h
@@ -0,0 +1,14 @@
+#pragma once
+#include <string>
+
+using namespace std;
+
+namespace Snake {
+namespace Sprites {
+class GridSprites {
+public:
+  static const string gridSprite1;
+  static const string gridSprite2;
+};
+} // namespace Sprites
+} // namespace Snake
