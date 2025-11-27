163b9226c679be8d5808bc3f0d72646551422dfd
Adise <admin@pauibanez.me>
2025-11-27 09:01:30 +0100

    created the Sprite class

---
+++ src/rendering/Sprite/Sprite.h
@@ -0,0 +1,17 @@
+#pragma once
+#include "<string>"
+
+
+
+using namespace std;
+
+namespace Snake {
+
+namespace Rendering {
+class Sprite {
+public:
+  u32String void Sprite()
+}
+} // namespace Rendering
+
+} // namespace Snake
