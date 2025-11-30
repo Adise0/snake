8c2e9a389aceca3e4171912d21dea92b49ddbfad
Adise <admin@pauibanez.me>
2025-11-30 18:10:53 +0100

    fixed idStore declaratioon

--- src/objects/Sprite/Sprite.cpp
+++ src/objects/Sprite/Sprite.cpp
@@ -1,6 +1,9 @@
 #include "Sprite.h"
 
+
 namespace Snake::Objects {
+int Sprite::idStore = 0;
+
 Sprite::Sprite(vector<u32string> characters) {
   this->id = GetNewId();
   this->characters = characters;
