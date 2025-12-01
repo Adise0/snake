a0e5788b580731c6de2bdccd223944b92ff576dd
Adise <admin@pauibanez.me>
2025-12-01 18:44:38 +0100

    added more definitions to gameManager and changed initialization structure

--- src/GameManager/GameManager.cpp
+++ src/GameManager/GameManager.cpp
@@ -9,8 +9,27 @@ CellType GameManager::cells[Consts::MAP_X][Consts::MAP_Y];
 std::queue<Vector2> GameManager::snake;
 
 void GameManager::Initialize() {
+  // #region Initialize
   Display::Initialize();
+  InitializeGrid();
 
+
+  Run();
+  // #endregion
+}
+
+void GameManager::InitializeGrid() {
+  // #region InitializeGrid
+  for (size_t y = 0; y < Consts::MAP_Y; y++) {
+    for (size_t x = 0; x < Consts::MAP_X; x++) {
+      cells[x][y] = CellType::Empty;
+    }
+  }
+  // #endregion
+}
+
+void GameManager::Run() {
+  // #region Run
   auto lastFrame = std::chrono::high_resolution_clock::now();
 
   while (true) {
@@ -21,6 +40,7 @@ void GameManager::Initialize() {
 
     Tick(deltaTime);
   }
+  // #endregion
 }
 
 void GameManager::Tick(float deltaTime) { Display::Tick(); }
--- src/GameManager/GameManager.h
+++ src/GameManager/GameManager.h
@@ -14,12 +14,29 @@ private:
   /// @brief The snake queue
   static std::queue<Vector2> snake;
 
-  /// @brief The tick method. Ticks every frame
-  /// @param deltaTime The delta time passed since last frame
-  static void Tick(float deltaTime);
 
 public:
   /// @brief Initializes the gameManager
   static void Initialize();
+
+private:
+  /// @brief The tick method. Ticks every frame
+  /// @param deltaTime The delta time passed since last frame
+  static void Tick(float deltaTime);
+
+  /// @brief Initializes the grid
+  static void InitializeGrid();
+
+  /// @brief Runs the tick system
+  static void Run();
+
+  /// @brief Gets a new safe apple position
+  /// @return The new apple position
+  static Vector2 GetNewApplePossition();
+
+  /// @brief Checks if the given cell is safe. (Not a wall or snake body)
+  /// @param cell The cell to check
+  /// @return Whether it's safe
+  static bool IsCellSafe(Vector2 cell);
 };
 } // namespace Snake
