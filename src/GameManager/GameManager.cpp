fffd3f04469acdbd0fb61a2bf9525c45cfe9029c
Adise <admin@pauibanez.me>
2025-12-11 12:15:00 +0100

    prevented .old prefixed extensions from being compiled

---
+++ src/GameManager/GameManager.cpp
@@ -0,0 +1,11 @@
+#include "GameManager.h"
+#include "../data/Sprites/Sprites.h"
+#include "../rendering/Display/Display.h"
+#include <algorithm>
+#include <chrono>
+#include <iostream>
+
+using namespace Snake;
+using namespace Rendering;
+using namespace Data;
+
